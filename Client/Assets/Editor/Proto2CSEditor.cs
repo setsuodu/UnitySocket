b39d7a6f744ed68ac3ebdb8d48a904953e647cdb
setsuodu <339131001@qq.com>
2020-01-28 04:08:46 +0800

    add proto

---
+++ Client/Assets/Editor/Proto2CSEditor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Path = System.IO.Path;
+using UnityEngine;
+using UnityEditor;
+
+internal class OpcodeInfo
+{
+	public string Name;
+	public int Opcode;
+}
+
+public class Proto2CSEditor : EditorWindow
+{
+	[MenuItem("Tools/Proto2CS")]
+	public static void AllProto2CS()
+	{
+		Process process = ProcessHelper.Run("dotnet", "Proto2CS.dll", "../Proto/", true);
+		UnityEngine.Debug.Log(process.StandardOutput.ReadToEnd());
+		AssetDatabase.Refresh();
+	}
+}
+
+public static class ProcessHelper
+{
+    public static Process Run(string exe, string arguments, string workingDirectory = ".", bool waitExit = false)
+    {
+        try
+        {
+            bool redirectStandardOutput = true;
+            bool redirectStandardError = true;
+            bool useShellExecute = false;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                redirectStandardOutput = false;
+                redirectStandardError = false;
+                useShellExecute = true;
+            }
+
+            if (waitExit)
+            {
+                redirectStandardOutput = true;
+                redirectStandardError = true;
+                useShellExecute = false;
+            }
+
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = exe,
+                Arguments = arguments,
+                CreateNoWindow = true,
+                UseShellExecute = useShellExecute,
+                WorkingDirectory = workingDirectory,
+                RedirectStandardOutput = redirectStandardOutput,
+                RedirectStandardError = redirectStandardError,
+            };
+
+            Process process = Process.Start(info);
+
+            if (waitExit)
+            {
+                process.WaitForExit();
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception($"{process.StandardOutput.ReadToEnd()} {process.StandardError.ReadToEnd()}");
+                }
+            }
+
+            return process;
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"dir: {Path.GetFullPath(workingDirectory)}, command: {exe} {arguments}", e);
+        }
+    }
+}
---
+++ Client/Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+using Google.Protobuf;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        #region Class序列化成二进制
+        TheMsg msg = new TheMsg();
+        msg.Name = "am the name";
+        msg.Num = 32;
+        Debug.Log(string.Format("The Msg is ( Name:{0}, Num:{1} )", msg.Name, msg.Num));
+
+        byte[] bmsg;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            msg.WriteTo(ms);
+            bmsg = ms.ToArray();
+        }
+        #endregion
+
+        #region 二进制反序列化成Class
+        TheMsg msg2 = TheMsg.Parser.ParseFrom(bmsg);
+        Debug.Log(string.Format("The Msg2 is ( Name:{0}, Num:{1} )", msg2.Name, msg2.Num));
+        #endregion
+    }
+}
